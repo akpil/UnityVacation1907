20560629985f50909f1578d298c953411e2d74e0
Akpil <jyk0802@naver.com>
2019-07-09 11:54:28 +0900

    GetKey, GetButton, GetAxis

---
+++ Roll A Ball/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        Debug.Log("Horizontal = " + Horizontal.ToString());
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Booster");
+        }
+    }
+}
