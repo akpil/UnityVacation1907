778e54dc77e3e624ae6e5b50147980eb38245c79
Akpil <jyk0802@naver.com>
2019-07-10 10:52:23 +0900

    Set Player

--- Roll A Ball/Assets/Player.cs
+++ Roll A Ball/Assets/Player.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody mRB;
+    public float Speed;
     // Start is called before the first frame update
     void Start()
     {
+        mRB = GetComponent<Rigidbody>();
         Debug.Log("Hello world!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal");
-        Debug.Log("Horizontal = " + Horizontal.ToString());
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 power = new Vector3(horizontal * Speed, 0, vertical * Speed);
+
+        //mRB.AddForce(power);
+        mRB.velocity = power;
+
         if (Input.GetButtonDown("Jump"))
         {
             Debug.Log("Booster");
