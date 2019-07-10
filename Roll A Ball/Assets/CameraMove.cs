5a7b53a5040ed680852f5257e1e21fbf59f13512
Akpil <jyk0802@naver.com>
2019-07-10 11:59:54 +0900

    Camera move, Pickup

---
+++ Roll A Ball/Assets/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject mPlayerObj;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = mPlayerObj.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mPlayerObj.transform.position - offset;
+    }
+}
---
+++ Roll A Ball/Assets/PickUP.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUP : MonoBehaviour
+{
+    public Vector3 mTumble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(mTumble * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("On trigger Enter!!");
+        }
+    }
+}
