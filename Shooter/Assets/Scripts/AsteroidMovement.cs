cd2e2ae9af627382ec0d79c06040bcb24521123f
Akpil <jyk0802@naver.com>
2019-07-16 11:53:01 +0900

    BG scrolling, Asteroid

---
+++ Shooter/Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour
+{
+    private Rigidbody mRB;
+    public float Speed;
+    private void Awake()
+    {
+        mRB = GetComponent<Rigidbody>();
+        mRB.velocity = Vector3.back * Speed;
+    }
+    private void OnEnable()
+    {
+        mRB.angularVelocity = Random.onUnitSphere * 3;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bolt") ||
+            other.gameObject.CompareTag("Player"))
+        {
+            //점수 올리기
+            //이펙트
+            //사운드
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Shooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    private Rigidbody mRB;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB = GetComponent<Rigidbody>();
+        mRB.velocity = Vector3.back * Speed;
+    }
+
+    public void SetSpeed(float inputSpeed)
+    {
+        Speed = inputSpeed;
+        mRB.velocity = Vector3.back * Speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bumper"))
+        {
+            transform.position = transform.position + (Vector3.forward * 40.96f);
+        }
+    }
+}
