30f0d782e8646c2f4dd22f67906875b72a122a8e
Akpil <jyk0802@naver.com>
2019-07-17 10:54:37 +0900

    Asteroid pool

---
+++ Shooter/Assets/Scripts/AsteroidPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidPool : MonoBehaviour
+{
+    public AsteroidMovement[] PrefabArr;
+    private List<AsteroidMovement>[] PoolArr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolArr = new List<AsteroidMovement>[PrefabArr.Length];
+        for (int i = 0; i < PoolArr.Length; i++)
+        {
+            PoolArr[i] = new List<AsteroidMovement>();
+        }
+    }
+
+    public AsteroidMovement GetFromPool(int id)
+    {
+        for (int i = 0; i < PoolArr[id].Count; i++)
+        {
+            if (!PoolArr[id][i].gameObject.activeInHierarchy)
+            {
+                PoolArr[id][i].gameObject.SetActive(true);
+                return PoolArr[id][i];
+            }
+        }
+        AsteroidMovement newObj = Instantiate(PrefabArr[id]);
+        PoolArr[id].Add(newObj);
+        return newObj;
+    }
+}
---
+++ Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public AsteroidPool asteroidPool;
+    public float SpawnZPos;
+    public float SpawnXMin;
+    public float SpawnXMax;
+    public int AstSpawnCount;
+    public float SpawnPeriod;
+    private float currentSpawnPeriod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpawnPeriod = SpawnPeriod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpawnPeriod = currentSpawnPeriod - Time.deltaTime;
+        if (currentSpawnPeriod <= 0)
+        {
+            for (int i = 0; i < AstSpawnCount; i++)
+            {
+                AsteroidMovement ast = asteroidPool.GetFromPool(Random.Range(0,3));
+                ast.transform.position = new Vector3(Random.Range(SpawnXMin, SpawnXMax),
+                                                     0,
+                                                     SpawnZPos);
+            }
+            currentSpawnPeriod = SpawnPeriod;
+        }
+    }
+}
