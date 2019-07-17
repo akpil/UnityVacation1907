0df3cff5ca2fe67facfa83558687f55f6ee829e4
Akpil <jyk0802@naver.com>
2019-07-17 12:10:41 +0900

    Coroutine, Enemy setup

---
+++ Shooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody mRB;
+    public Transform BoltPos;
+    public BoltPool pool;
+
+    void Awake()
+    {
+        mRB = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(AutoFire());
+    }
+
+    private IEnumerator AutoFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.7f, 1.2f));
+            Bolt bolt = pool.GetFromPool();
+            bolt.transform.position = BoltPos.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bolt") ||
+            other.gameObject.CompareTag("Player"))
+        {
+            //점수 올리기
+            //이펙트
+            //사운드
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Shooter/Assets/Scripts/GameController.cs
+++ Shooter/Assets/Scripts/GameController.cs
@@ -9,29 +9,32 @@ public class GameController : MonoBehaviour
     public float SpawnXMin;
     public float SpawnXMax;
     public int AstSpawnCount;
-    public float SpawnPeriod;
-    private float currentSpawnPeriod;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentSpawnPeriod = SpawnPeriod;
+        StartCoroutine(SpawnHazard());
+    }
+
+    private IEnumerator SpawnHazard()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(3);
+            for (int i = 0; i < AstSpawnCount; i++)
+            {
+                AsteroidMovement ast = asteroidPool.GetFromPool(Random.Range(0, 3));
+                ast.transform.position = new Vector3(Random.Range(SpawnXMin, SpawnXMax),
+                                                     0,
+                                                     SpawnZPos);
+                yield return new WaitForSeconds(.4f);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentSpawnPeriod = currentSpawnPeriod - Time.deltaTime;
-        if (currentSpawnPeriod <= 0)
-        {
-            for (int i = 0; i < AstSpawnCount; i++)
-            {
-                AsteroidMovement ast = asteroidPool.GetFromPool(Random.Range(0,3));
-                ast.transform.position = new Vector3(Random.Range(SpawnXMin, SpawnXMax),
-                                                     0,
-                                                     SpawnZPos);
-            }
-            currentSpawnPeriod = SpawnPeriod;
-        }
+        
     }
 }
