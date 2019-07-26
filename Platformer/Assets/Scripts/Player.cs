b1febc6e2c27933df3bfe7a9aeac2ca52e063afe
Akpil <jyk0802@naver.com>
2019-07-26 11:54:14 +0900

    animation script

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator mAnim;
+    public int WalkHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnim = GetComponent<Animator>();
+        WalkHash = Animator.StringToHash("IsWalk");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            mAnim.SetBool(WalkHash, true);
+        }
+        else if (horizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            mAnim.SetBool(WalkHash, true);
+        }
+        else
+        {
+            mAnim.SetBool(WalkHash, false);
+        }
+        
+    }
+}
