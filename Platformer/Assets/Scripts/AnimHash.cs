e2117accd28704f7760dc4fd0be92b89ca1c0392
Akpil <jyk0802@naver.com>
2019-07-29 11:52:06 +0900

    Jump

--- Platformer/Assets/Scripts/AnimHash.cs
+++ Platformer/Assets/Scripts/AnimHash.cs
@@ -6,4 +6,5 @@ public static class AnimHash
 {
     public static readonly int Walk = Animator.StringToHash("IsWalk");
     public static readonly int Melee = Animator.StringToHash("IsMelee");
+    public static readonly int Jump = Animator.StringToHash("Jump");
 }
--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animator mAnim;
+    private Rigidbody2D mRB2D;
+    public float Speed;
+    public float JumpSpeed;
+
+    private bool mbGround;
+
     // Start is called before the first frame update
     void Start()
     {
         mAnim = GetComponent<Animator>();
+        mRB2D = GetComponent<Rigidbody2D>();
+        mbGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-
         if (horizontal > 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -30,6 +37,14 @@ public class Player : MonoBehaviour
         {
             mAnim.SetBool(AnimHash.Walk, false);
         }
+        mRB2D.velocity = new Vector2(horizontal * Speed, mRB2D.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && mbGround)
+        {
+            mRB2D.velocity = mRB2D.velocity + new Vector2(0, JumpSpeed);
+            mbGround = false;
+        }
+        mAnim.SetFloat(AnimHash.Jump, mRB2D.velocity.y);
 
         if (Input.GetButtonDown("Fire1"))
         {
