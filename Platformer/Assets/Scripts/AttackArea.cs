96e869108008fc643b9d761f9a9a2684793c9e82
Akpil <jyk0802@naver.com>
2019-07-30 11:52:29 +0900

    Melee attack(50%)

---
+++ Platformer/Assets/Scripts/AttackArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackArea : MonoBehaviour
+{
+    public string TargetTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TargetTag))
+        {
+            collision.SendMessage("Hit", 1, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/HitBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public int TargetHitCount;
+    public int currentHitCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitCount = 0;
+    }
+
+    public void Hit(int value)
+    {
+        currentHitCount++;
+        if (currentHitCount == TargetHitCount)
+        {
+            StartCoroutine(CheckUP());
+        }
+    }
+
+    private IEnumerator CheckUP()
+    {
+        yield return new WaitForSeconds(5);
+        if (currentHitCount == TargetHitCount)
+        {
+            Debug.Log("to NextStage");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
