c3fd36b2cd0a9190a329e11608c97e135c7f46fd
Akpil <jyk0802@naver.com>
2019-08-01 11:54:48 +0900

    Enemy

---
+++ Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D mRB2D;
+    private Animator mAnim;
+
+    public float MoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRB2D = GetComponent<Rigidbody2D>();
+        mAnim = GetComponent<Animator>();
+
+        mAnim.SetBool(AnimHash.Walk, true);
+        mRB2D.velocity = transform.right * MoveSpeed;
+    }
+
+
+}
