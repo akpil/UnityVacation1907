67f04825c0414dfa82494bf0d5658551be3c601c
Akpil <jyk0802@naver.com>
2019-08-08 11:55:58 +0900

    Scroll UI

---
+++ Platformer/Assets/Scripts/UIElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIElement : MonoBehaviour
+{
+    public Button PurchaseButton;
+    public Text TitleText;
+    public Text ContentsText;
+    public Text CostText;
+    public Text PurchaseText;
+    public Image Icon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
